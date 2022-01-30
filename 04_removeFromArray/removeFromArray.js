c4b814b7e51faa6bd05bb56d97070080d3b0137a
John-Chiu <29536154+John-Chiu@users.noreply.github.com>
2022-01-30 17:57:27 +0800

    finish 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...rest) {
+    for (const target of rest) {
+        arr = arr.filter(item => item !== target);
+    }
+    return arr;
 };
 
 // Do not edit below this line
