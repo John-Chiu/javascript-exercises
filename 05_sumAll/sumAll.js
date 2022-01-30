a56f70bc32ac1920b0b86a4fa65dcdd867fb50cc
John-Chiu <29536154+John-Chiu@users.noreply.github.com>
2022-01-30 18:38:35 +0800

    finish 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(...rest) {
+    // return "ERROR" if any parameter is: not number, negative
+    for (const item of rest) 
+        if (typeof item != "number" || item < 0) 
+            return "ERROR";
 
+    let min = Math.min(...rest);
+    let max = Math.max(...rest);
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
