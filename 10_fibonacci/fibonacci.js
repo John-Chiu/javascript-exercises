9dc91ff85405d52f4f00e1f9a902178de84f006a
John-Chiu <29536154+John-Chiu@users.noreply.github.com>
2022-02-04 18:12:55 +0800

    finish 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(th) {
+    if (th < 0)
+        return "OOPS";
 
+    if (th == 0)
+        return 0;
+    if (th == 1)
+        return 1;
+
+    let num1 = 0;
+    let num2 = 1;
+    let num3;
+    for (let i = 1; i < th; i++) {
+        num3 = num1 + num2;
+        num1 = num2;
+        num2 = num3;
+    }
+    return num3;
 };
 
 // Do not edit below this line
