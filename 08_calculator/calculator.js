d01be91b722ae7148b79c0d73e7f9fce44c9291b
John-Chiu <29536154+John-Chiu@users.noreply.github.com>
2022-02-04 16:07:45 +0800

    finish 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((sum, item) => sum + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, item) => total * item, 1);
 };
 
-const power = function() {
-	
+const power = function(num, times) {
+  if (times === 0)
+    return 1;
+
+  let total = 1;
+  for (let i = 0; i < times; i++) {
+    total *= num;
+  }
+	return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(fact) {
+	let total = 1;
+  for (let i = 1; i <= fact; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
