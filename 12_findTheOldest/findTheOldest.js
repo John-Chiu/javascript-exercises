106912b3f962b09f22c3abc7469b7a5c04defafb
John-Chiu <29536154+John-Chiu@users.noreply.github.com>
2022-02-04 19:01:03 +0800

    finish 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    arr.sort((a, b) => {
+        const aAge = getAge(a);
+        const bAge = getAge(b);
+        return aAge - bAge;
+    });
+    return arr[arr.length - 1];
 };
+
+function getAge(person) {
+    let age;
+    if ("yearOfDeath" in person)
+        age = person.yearOfDeath - person.yearOfBirth;
+    else
+        age = new Date().getFullYear() - person.yearOfBirth;
+    return age;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
