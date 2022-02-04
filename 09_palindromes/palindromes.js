e9edec01a3cbfc7301940539d1cf6b898ed18b0f
John-Chiu <29536154+John-Chiu@users.noreply.github.com>
2022-02-04 17:26:55 +0800

    finish 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let l = 0;
+    let r = str.length - 1;
 
+    while (l < r) {
+        if (!isLetter(str[l]))
+            l++;
+        else if (!isLetter(str[r]))
+            r--;
+        // check if they are same letter, ignore case
+        else if (str[l].toUpperCase() === str[r].toUpperCase()) {
+            l++;
+            r--;
+        } else
+            return false;
+    }
+    return true;
 };
+
+function isLetter(ch) {
+    return ch.toLowerCase() !== ch.toUpperCase();
+}
 
 // Do not edit below this line
 module.exports = palindromes;
